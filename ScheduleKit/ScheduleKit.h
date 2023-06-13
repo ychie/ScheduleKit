fb604bbce5186ff30cb22a0bbf1f5d5981082e82
ychie <ychie@Nazars-MacBook-Air.local>
2023-06-13 15:03:24 +0300

    Project init

---
+++ ScheduleKit/ScheduleKit.h
@@ -0,0 +1,18 @@
+//
+//  ScheduleKit.h
+//  ScheduleKit
+//
+//  Created by Nazar Voitovych on 13.06.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScheduleKit.
+FOUNDATION_EXPORT double ScheduleKitVersionNumber;
+
+//! Project version string for ScheduleKit.
+FOUNDATION_EXPORT const unsigned char ScheduleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScheduleKit/PublicHeader.h>
+
+
